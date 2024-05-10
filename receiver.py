3f04a281060181ddbd8c665b478b347fbd40b1b5
Nianhong <nianhong@kairosaerospace.com>
2024-05-10 15:27:40 -0700

    basic send and receive

--- receiver.py
+++ receiver.py
@@ -1,31 +1,24 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import socket
 
 app = Flask(__name__)
 
 UDP_IP = "0.0.0.0"  # Listen on all available interfaces
-UDP_PORT = 5005
+UDP_PORT = 5007
 
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/data')
 def get_data():
-    # Create a UDP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((UDP_IP, UDP_PORT))
-
     # Receive UDP broadcast data
     data, addr = sock.recvfrom(1024)
-
-    # Close the socket
-    sock.close()
-
-    return jsonify({'data': data.decode('utf-8')})
-
+    return data.decode('utf-8')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
--- sender.py
+++ sender.py
@@ -2,7 +2,7 @@ import socket
 import time
 
 UDP_IP = "255.255.255.255"  # Broadcast IP address
-UDP_PORT = 5005
+UDP_PORT = 5007
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -10,15 +10,17 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 i = 0
 while True:
-    message = input("Enter message to broadcast (type 'exit' to quit): ")
-    # t = time.time()
-    # fmt = time.gmtime(t)
-    # message = time.strftime("%D, %T", fmt)
-    if message.lower() == 'exit':
-        break
+    # message = input("Enter message to broadcast (type 'exit' to quit): ")
+    t = time.time()
+    fmt = time.gmtime(t)
+    message = time.strftime("%D, %T", fmt)
+    # if message.lower() == 'exit':
+    #     break
 
     # Send data via UDP broadcast
     sock.sendto(message.encode('utf-8'), (UDP_IP, UDP_PORT))
+    print("send:", message)
+    time.sleep(2)
 
 # Close the socket
 sock.close()
